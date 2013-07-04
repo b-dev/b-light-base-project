98f38223f8d275364354ce76517de92a1262bc4d
Marco Minutoli <info@marcominutoli.it>
2013-07-05 00:41:14 +0200

    fix default db settings and cache for production

--- website/settings/production.py
+++ website/settings/production.py
@@ -48,13 +48,26 @@ SERVER_EMAIL = EMAIL_HOST_USER
 ########## END EMAIL CONFIGURATION
 
 ########## DATABASE CONFIGURATION
-DATABASES = {}
+DATABASES = {
+    'default': {
+        'ENGINE': 'django.db.backends.%s' % PRJ_ENGINE,
+        'NAME': PRJ_DB,
+        'USER': PRJ_USER,
+        'PASSWORD': PRJ_PASS,
+        'HOST': 'localhost',
+        'PORT': '',
+    }
+}
 ########## END DATABASE CONFIGURATION
 
 
 ########## CACHE CONFIGURATION
 # See: https://docs.djangoproject.com/en/dev/ref/settings/#caches
-CACHES = {}
+CACHES = {
+    'default': {
+        'BACKEND': 'django.core.cache.backends.locmem.LocMemCache',
+    }
+}
 ########## END CACHE CONFIGURATION
 
 
