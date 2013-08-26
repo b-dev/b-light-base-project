f2f3d58704a5d1e56c94b2cbf0b0d9c94648d416
Marco Minutoli <info@marcominutoli.it>
2013-08-26 16:00:08 +0200

    corretto ROOT PATH templates directory

--- website/settings/base.py
+++ website/settings/base.py
@@ -165,7 +165,7 @@ TEMPLATE_LOADERS = (
 
 # See: https://docs.djangoproject.com/en/dev/ref/settings/#template-dirs
 TEMPLATE_DIRS = (
-    normpath(join(SITE_ROOT, 'templates')),
+    normpath(join(DJANGO_ROOT, 'templates')),
 )
 ########## END TEMPLATE CONFIGURATION
 
